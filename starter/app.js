8bf9295f6eeef106194f5efa8aad3f02a08ab5d6
vagrant <b.93.kenny@gmail.com>
2020-10-24 02:15:44 +0000

    created a getInput method to get the values from the input fields in the html

--- starter/app.js
+++ starter/app.js
@@ -7,21 +7,32 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-
+  return {
+    getInput: function() {
+      return {
+        type : document.querySelector('.add__type').value, // will be either inc (income) or exp (expense).
+        description : document.querySelector('.add__description').value,
+        value : document.querySelector('.add__value').value
+      }
+    }
+  };
 
 })();
 
 // GLOBAL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  document.querySelector('.add__btn').addEventListener('click', function() {
-    console.log('clicked')
-  });
+  var ctrlAddItem = function() {
+
+    }
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
 
     if(event.keyCode === 13 || event.which === 13) {
-      console.log('enter was pressed')
+      ctrlAddItem();
     }
 
   });
