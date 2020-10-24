7f7a5125f9a8c15776a9f037df8bed32e6024749
vagrant <b.93.kenny@gmail.com>
2020-10-24 06:10:14 +0000

    created a DOMStrings object to store all the field input data in

--- starter/app.js
+++ starter/app.js
@@ -7,13 +7,24 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
   return {
     getInput: function() {
       return {
-        type : document.querySelector('.add__type').value, // will be either inc (income) or exp (expense).
-        description : document.querySelector('.add__description').value,
-        value : document.querySelector('.add__value').value
-      }
+        type : document.querySelector(DOMStrings.inputType).value, // will be either inc (income) or exp (expense).
+        description : document.querySelector(DOMStrings.inputDescription).value,
+        value : document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+
+    getDOMStrings: function() {
+      return DOMStrings;
     }
   };
 
@@ -22,12 +33,21 @@ var UIController = (function() {
 // GLOBAL CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMStrings();
+
   var ctrlAddItem = function() {
 
-    }
-  }
+    // 1. get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+    // 2. add the item to the budget controller
+    // 3. add the item to the UI
+    // 4. calculate the budget
+    // 5. display the budget on the UI
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    }
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
   document.addEventListener('keypress', function(event) {
 
